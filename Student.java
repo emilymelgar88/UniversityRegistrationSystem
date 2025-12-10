eb4553db963ab6c8b422ec160f2be49ae0213c38
emilymelgar88 <emilymelgar@cpp.edu>
2025-12-09 18:55:10 -0800

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,34 @@
+// Student.java
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Student {
+private String id;
+private String name;
+private List<Course> enrolledCourses = new ArrayList<>();
+
+
+public Student(String id, String name) {
+this.id = id.trim();
+this.name = name.trim();
+}
+
+
+public String getId() { return id; }
+public String getName() { return name; }
+
+
+public List<Course> getEnrolledCourses() { return enrolledCourses; }
+
+
+public void enroll(Course c) {
+enrolledCourses.add(c);
+}
+
+
+@Override
+public String toString() {
+return id + ", " + name;
+}
+}
